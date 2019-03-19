7d532fdc0f6042dfe8ef8991d7ccf3882b6acdd1
Onur Genes <onur@onurgenes.com>
2019-03-19 10:53:17 +0300

    Added OAuthSwift and example

--- OAuthExample/ViewController.swift
+++ OAuthExample/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import OAuthSwift
 
 class ViewController: UIViewController {
 
+    var oauthSwift: OAuth1Swift?
+    var handle: OAuthSwiftRequestHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        oauthSwift = OAuth1Swift(consumerKey: "YOUR_CONSUMER_KEY",
+                                 consumerSecret: "YOUR_CONSUMER SECRET",
+                                 requestTokenUrl: "https://api.twitter.com/oauth/request_token",
+                                 authorizeUrl: "https://api.twitter.com/oauth/authorize",
+                                 accessTokenUrl: "https://api.twitter.com/oauth/access_token")
 
+        handle = oauthSwift!.authorize(withCallbackURL: "oauth-example://oauth-callback/twitter", success: { (credential, response, params) in
+            print("CREDENTIAL : \(credential)")
+            print("RESPONSE : \(response)")
+            print("PARAMS : \(params)")
+        }) { (error) in
+            print(error.description)
+        }
+    
+    }
 
 }
 
